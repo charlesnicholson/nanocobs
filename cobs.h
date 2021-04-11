50a27d3ce42aac0ef816a5af6f63365f990de3b2
Charles Nicholson <charles.nicholson@gmail.com>
2021-04-11 13:25:55 -0400

    add signatures for inplace vs external, break up tests

--- cobs.h
+++ cobs.h
@@ -7,16 +7,28 @@ typedef enum {
 } cobs_ret_t;
 
 enum {
-  COBS_SENTINEL_VALUE = 0x5A,
-  COBS_SAFE_BUFFER_SIZE = 256
+  COBS_INPLACE_SENTINEL_VALUE = 0x5A,
+  COBS_INPLACE_SAFE_BUFFER_SIZE = 256
 };
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-cobs_ret_t cobs_encode(void *buf, unsigned len);
-cobs_ret_t cobs_decode(void *buf, unsigned len);
+cobs_ret_t cobs_encode_inplace(void *buf, unsigned len);
+cobs_ret_t cobs_decode_inplace(void *buf, unsigned len);
+
+cobs_ret_t cobs_encode(void const *src,
+                       unsigned src_len,
+                       void *dst,
+                       unsigned dst_len,
+                       unsigned *out_encoded_len);
+
+cobs_ret_t cobs_decode(void const *src,
+                       unsigned src_len,
+                       void *dst,
+                       unsigned dst_len,
+                       unsigned *out_decoded_len);
 
 #ifdef __cplusplus
 }
