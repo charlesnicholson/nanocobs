fe17daea3b9191fdc1e4fa88d008919acff0376f
Charles Nicholson <charles.nicholson@gmail.com>
2021-04-22 07:39:41 -0400

    tests for the figures in the cobs paper

---
+++ tests/test_paper_figures.cc
@@ -0,0 +1,47 @@
+#include "../cobs.h"
+#include "catch.hpp"
+
+using byte_vec_t = std::vector< unsigned char >;
+
+// http://www.stuartcheshire.org/papers/COBSforToN.pdf
+TEST_CASE("COBS paper examples", "[cobs_encode][cobs_decode]") {
+  SECTION("Figure 2") {
+    byte_vec_t input(680, 0x01);
+    input[input.size() - 1] = 0x00;
+
+    byte_vec_t output(684);
+    unsigned output_len;
+
+    REQUIRE( cobs_encode(input.data(),
+                         static_cast< unsigned >(input.size()),
+                         output.data(),
+                         static_cast< unsigned >(output.size()),
+                         &output_len) == COBS_RET_SUCCESS );
+
+
+    byte_vec_t expected{0xFF};
+    expected.insert(std::end(expected), 254, 0x01);
+    expected.push_back(0xFF);
+    expected.insert(std::end(expected), 254, 0x01);
+    expected.push_back(0xAC);
+    expected.insert(std::end(expected), 172, 0x01);
+    expected.push_back(0x00);
+    REQUIRE( output == expected );
+  }
+
+  SECTION("Figure 3") {
+    byte_vec_t input {0x45,0x00,0x00,0x2C,0x4C,0x79,0x00,0x00,0x40,0x06,0x4F,0x37};
+    byte_vec_t output
+        {0x02,0x45,0x01,0x04,0x2C,0x4C,0x79,0x01,0x05,0x40,0x06,0x4F,0x37,0x00};
+
+    char encoded[256];
+    unsigned encoded_len;
+
+    REQUIRE( cobs_encode(input.data(),
+                         static_cast< unsigned >(input.size()),
+                         encoded,
+                         sizeof(encoded),
+                         &encoded_len) == COBS_RET_SUCCESS );
+    REQUIRE( output == byte_vec_t(encoded, encoded + encoded_len) );
+  }
+}
