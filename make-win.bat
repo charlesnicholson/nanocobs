52b3224434503ade8feee02a529a747197a221ba
Charles Nicholson <charles.nicholson@gmail.com>
2021-04-24 16:04:07 -0400

    windows presubmit (#13)
    * windows presubmit
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ make-win.bat
@@ -0,0 +1,11 @@
+cl.exe /W4 /EHsc ^
+    cobs.c ^
+    tests/test_cobs_encode_max.cc ^
+    tests/test_cobs_encode.cc ^
+    tests/test_cobs_encode_inplace.cc ^
+    tests/test_cobs_decode_inplace.cc ^
+    tests/test_paper_figures.cc ^
+    tests/test_wikipedia.cc ^
+    tests/unittest_main.cc ^
+    /link /out:cobs_unittests.exe || exit /b 1
+cobs_unittests.exe || exit /b 1
--- tests/test_cobs_encode_inplace.cc
+++ tests/test_cobs_encode_inplace.cc
@@ -1,10 +1,13 @@
 #include "../cobs.h"
 #include "catch.hpp"
 
+#include <numeric>
+
 #include <cstring>
 
-using byte_vec_t = std::vector< unsigned char >;
-static constexpr unsigned char CSV = COBS_INPLACE_SENTINEL_VALUE;
+using byte_t = unsigned char;
+using byte_vec_t = std::vector< byte_t >;
+static constexpr byte_t CSV = COBS_INPLACE_SENTINEL_VALUE;
 
 namespace {
   cobs_ret_t cobs_encode_vec(byte_vec_t &v) {
@@ -78,26 +81,26 @@ TEST_CASE("Inplace encoding", "[cobs_encode_inplace]") {
 
   SECTION("Safe payload, all zero bytes") {
     byte_vec_t buf(COBS_INPLACE_SAFE_BUFFER_SIZE);
-    std::fill(std::begin(buf), std::end(buf), 0x00);
+    std::fill(std::begin(buf), std::end(buf), static_cast< byte_t >(0x00));
     buf[0] = CSV;
     buf[buf.size() - 1] = CSV;
     REQUIRE( cobs_encode_vec(buf) == COBS_RET_SUCCESS );
 
     byte_vec_t expected(buf.size());
-    std::fill(std::begin(expected), std::end(expected), 0x01);
+    std::fill(std::begin(expected), std::end(expected), static_cast< byte_t >(0x01));
     expected[expected.size() - 1] = 0x00;
     REQUIRE( buf == expected );
   }
 
   SECTION("Safe payload, no zero bytes") {
     byte_vec_t buf(COBS_INPLACE_SAFE_BUFFER_SIZE);
-    std::iota(std::begin(buf), std::end(buf), 0x00);
+    std::iota(std::begin(buf), std::end(buf), static_cast< byte_t >(0x00));
     buf[0] = CSV;
     buf[buf.size() - 1] = CSV;
     REQUIRE( cobs_encode_vec(buf) == COBS_RET_SUCCESS );
 
     byte_vec_t expected(buf.size());
-    std::iota(std::begin(expected), std::end(expected), 0x00);
+    std::iota(std::begin(expected), std::end(expected), static_cast< byte_t >(0x00));
     expected[0] = 0xFF;
     expected[expected.size() - 1] = 0x00;
     REQUIRE( buf == expected );
