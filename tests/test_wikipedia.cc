27317f3755abfe7fab121a51a6130796c6c76805
Charles Nicholson <charles.nicholson@gmail.com>
2021-04-24 15:39:15 -0400

    wip

--- tests/test_wikipedia.cc
+++ tests/test_wikipedia.cc
@@ -1,6 +1,8 @@
 #include "../cobs.h"
 #include "catch.hpp"
 
+#include <numeric>
+
 using byte_vec_t = std::vector< unsigned char >;
 static constexpr unsigned char CSV = COBS_INPLACE_SENTINEL_VALUE;
 
