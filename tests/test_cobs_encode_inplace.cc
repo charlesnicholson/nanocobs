8156caf4c16b27299e2ef0b43cc368e62f3919aa
Charles Nicholson <charles.nicholson@gmail.com>
2021-04-29 08:26:48 -0400

    replace static casts with brace initialization (#17)

--- tests/test_cobs_encode_inplace.cc
+++ tests/test_cobs_encode_inplace.cc
@@ -81,26 +81,26 @@ TEST_CASE("Inplace encoding", "[cobs_encode_inplace]") {
 
   SECTION("Safe payload, all zero bytes") {
     byte_vec_t buf(COBS_INPLACE_SAFE_BUFFER_SIZE);
-    std::fill(std::begin(buf), std::end(buf), static_cast< byte_t >(0x00));
+    std::fill(std::begin(buf), std::end(buf), byte_t{0x00});
     buf[0] = CSV;
     buf[buf.size() - 1] = CSV;
     REQUIRE( cobs_encode_vec(buf) == COBS_RET_SUCCESS );
 
     byte_vec_t expected(buf.size());
-    std::fill(std::begin(expected), std::end(expected), static_cast< byte_t >(0x01));
+    std::fill(std::begin(expected), std::end(expected), byte_t{0x01});
     expected[expected.size() - 1] = 0x00;
     REQUIRE( buf == expected );
   }
 
   SECTION("Safe payload, no zero bytes") {
     byte_vec_t buf(COBS_INPLACE_SAFE_BUFFER_SIZE);
-    std::iota(std::begin(buf), std::end(buf), static_cast< byte_t >(0x00));
+    std::iota(std::begin(buf), std::end(buf), byte_t{0x00});
     buf[0] = CSV;
     buf[buf.size() - 1] = CSV;
     REQUIRE( cobs_encode_vec(buf) == COBS_RET_SUCCESS );
 
     byte_vec_t expected(buf.size());
-    std::iota(std::begin(expected), std::end(expected), static_cast< byte_t >(0x00));
+    std::iota(std::begin(expected), std::end(expected), byte_t{0x00});
     expected[0] = 0xFF;
     expected[expected.size() - 1] = 0x00;
     REQUIRE( buf == expected );
