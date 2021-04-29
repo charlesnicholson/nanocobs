c57509a26476becf62128a0890f6863b2870d5f5
Charles Nicholson <charles.nicholson@gmail.com>
2021-04-29 08:24:35 -0400

    replace static casts with brace initialization

--- tests/test_wikipedia.cc
+++ tests/test_wikipedia.cc
@@ -88,11 +88,11 @@ TEST_CASE("Wikipedia round-trip examples",
   SECTION("Example 6") {
     // 01 02 03 ... FD FE
     byte_vec_t decoded(254);
-    std::iota(std::begin(decoded), std::end(decoded), static_cast< byte_t >(0x01));
+    std::iota(std::begin(decoded), std::end(decoded), byte_t{0x01});
 
     // FF 01 02 03 ... FD FE 00
     byte_vec_t encoded(255);
-    std::iota(std::begin(encoded), std::end(encoded), static_cast< byte_t >(0x00));
+    std::iota(std::begin(encoded), std::end(encoded), byte_t{0x00});
     encoded[0] = 0xFF;
     encoded.push_back(0x00);
 
@@ -103,7 +103,7 @@ TEST_CASE("Wikipedia round-trip examples",
   SECTION("Example 7") {
     // 00 01 02 ... FC FD FE
     byte_vec_t decoded(255);
-    std::iota(std::begin(decoded), std::end(decoded), static_cast< byte_t >(0x00));
+    std::iota(std::begin(decoded), std::end(decoded), byte_t{0x00});
 
     // 01 FF 01 02 ... FC FD FE 00
     byte_vec_t encoded{0x01, 0xFF};
@@ -119,11 +119,11 @@ TEST_CASE("Wikipedia round-trip examples",
   SECTION("Example 8") {
     // 01 02 03 ... FD FE FF
     byte_vec_t decoded(255);
-    std::iota(std::begin(decoded), std::end(decoded), static_cast< byte_t >(0x01));
+    std::iota(std::begin(decoded), std::end(decoded), byte_t{0x01});
 
     // FF 01 02 03 ... FD FE 02 FF 00
     byte_vec_t encoded(255);
-    std::iota(std::begin(encoded), std::end(encoded), static_cast< byte_t >(0x00));
+    std::iota(std::begin(encoded), std::end(encoded), byte_t{0x00});
     encoded[0] = 0xFF;
     encoded.insert(std::end(encoded), {0x02, 0xFF, 0x00});
 
@@ -133,12 +133,12 @@ TEST_CASE("Wikipedia round-trip examples",
   SECTION("Example 9") {
     // 02 03 04 ... FE FF 00
     byte_vec_t decoded(255);
-    std::iota(std::begin(decoded), std::end(decoded), static_cast< byte_t >(0x02));
+    std::iota(std::begin(decoded), std::end(decoded), byte_t{0x02});
     decoded[decoded.size() - 1] = 0x00;
 
     // FF 02 03 04 ... FE FF 01 01 00
     byte_vec_t encoded(255);
-    std::iota(std::begin(encoded), std::end(encoded), static_cast< byte_t >(0x01));
+    std::iota(std::begin(encoded), std::end(encoded), byte_t{0x01});
     encoded[0] = 0xFF;
     encoded.insert(std::end(encoded), {0x01, 0x01, 0x00});
 
@@ -148,13 +148,13 @@ TEST_CASE("Wikipedia round-trip examples",
   SECTION("Example 10") {
     // 03 04 05 ... FF 00 01
     byte_vec_t decoded(253);
-    std::iota(std::begin(decoded), std::end(decoded), static_cast< byte_t >(0x03));
+    std::iota(std::begin(decoded), std::end(decoded), byte_t{0x03});
     decoded.insert(decoded.end(), {0x00, 0x01});
 
     // FE 03 04 05 ... FF 02 01 00
     byte_vec_t encoded{0xFE};
     for (auto i = 0x03u; i <= 0xFF; ++i) {
-      encoded.push_back(static_cast< unsigned char >(i));
+      encoded.push_back(static_cast< byte_t >(i));
     }
     encoded.insert(encoded.end(), {0x02, 0x01, 0x00});
 
