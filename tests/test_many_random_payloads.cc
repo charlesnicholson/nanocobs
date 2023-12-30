2720c2f20e23e549b2125fa747b6331116828657
Charles Nicholson <charles.nicholson@gmail.com>
2023-12-30 18:12:15 -0500

    golf

--- tests/test_many_random_payloads.cc
+++ tests/test_many_random_payloads.cc
@@ -18,33 +18,29 @@ std::atomic<int> s_iterations{ 250u };
 TEST_CASE("many random payloads") {
   auto const thread_proc{ [](unsigned seed) {
     std::mt19937 mt{ seed };  // deterministic
-    byte_vec_t source(LEN), decoded(LEN), encoded(COBS_ENCODE_MAX(LEN));
+    byte_vec_t source(LEN), dec(LEN), enc(COBS_ENCODE_MAX(LEN));
 
     while (--s_iterations > 0) {
       std::generate(source.begin(), source.end(), [&]() { return byte_t(mt() & 0xFF); });
       memset(source.data() + 1000, 0xAA, 256 * 10);  // nonzero run
 
       size_t enc_len{ 0u };
-      REQUIRE(cobs_encode(source.data(),
-                          source.size(),
-                          encoded.data(),
-                          encoded.size(),
-                          &enc_len) == COBS_RET_SUCCESS);
+      REQUIRE(
+          cobs_encode(source.data(), source.size(), enc.data(), enc.size(), &enc_len) ==
+          COBS_RET_SUCCESS);
 
       REQUIRE(enc_len >= LEN);
       REQUIRE(enc_len <= COBS_ENCODE_MAX(LEN));
-      REQUIRE(std::none_of(encoded.data(),
-                           encoded.data() + enc_len - 1,
-                           [](byte_t const b) { return b == 0; }));
-      REQUIRE(encoded[enc_len - 1] == 0);
+      REQUIRE(
+          std::none_of(enc.data(), enc.data() + enc_len - 1, [](byte_t b) { return !b; }));
+      REQUIRE(enc[enc_len - 1] == 0);
 
       size_t dec_len{ 0u };
-      REQUIRE(
-          cobs_decode(encoded.data(), enc_len, decoded.data(), decoded.size(), &dec_len) ==
-          COBS_RET_SUCCESS);
+      REQUIRE(cobs_decode(enc.data(), enc_len, dec.data(), dec.size(), &dec_len) ==
+              COBS_RET_SUCCESS);
 
       REQUIRE(dec_len == LEN);
-      REQUIRE(source == decoded);
+      REQUIRE(source == dec);
     }
   } };
 
